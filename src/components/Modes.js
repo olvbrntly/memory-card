c78bdc65f67306786a61349045bdc1251762e5bc
olivia <olivia.brantley9@gmail.com>
2023-03-21 10:32:21 -0700

    renamed files

---
+++ src/components/Modes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Modes(){
+    return(
+        <div>
+            <button> EASY </button>
+            <button> HARD </button>
+        </div>
+    )
+}
+
+export default Modes
