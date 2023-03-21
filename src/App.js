1fda99bc66f1b16933be0b1dcccd223cf4301a93
olivia <olivia.brantley9@gmail.com>
2023-03-20 19:56:09 -0700

    example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import michael from './images/michael.png'
+import pam from './images/pam.png'
+
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <h1>MEMORY GAME</h1> {/* this will be in the dundermifflin font */}
       <div>
         <img src={michael} alt='michael scott'/>
+        <img src={pam} alt='michael scott'/>
+
       </div>
     </div>
   );
