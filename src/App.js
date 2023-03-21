ca34a880caad29a7c865a84bacbaafac07fa6341
olivia <olivia.brantley9@gmail.com>
2023-03-20 19:55:06 -0700

    example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import michael from './images/michael.png'
 
 function App() {
   return (
     <div className="App">
       <h1>MEMORY GAME</h1> {/* this will be in the dundermifflin font */}
+      <div>
+        <img src={michael} alt='michael scott'/>
+      </div>
     </div>
   );
 }
